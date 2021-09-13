a7e09f880d3a8051c77577c3a7c7da66e4adee86
Damian <przpiw@gmail.com>
2021-09-14 00:26:36 +0930

    initial

---
+++ build/inheritance/MatchReader.js
@@ -0,0 +1,15 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+// export class MatchReader extends CsvFileReader<MatchData> {
+//   mapRow(row: string[]): MatchData {
+//     return [
+//       dateStringToDate(row[0]),
+//       row[1],
+//       row[2],
+//       parseInt(row[3]),
+//       parseInt(row[4]),
+//       row[5] as MatchResult,
+//       row[6]
+//     ]
+//   }
+// }
